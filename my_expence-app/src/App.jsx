76d101c49c01f88a5085383be339cf0fe5aa6e23
Fareedaasif083 <fareedaasif8130@gmail.com>
2025-11-22 06:36:15 +0500

    Update Sidebar and App.jsx

--- my_expence-app/src/App.jsx
+++ my_expence-app/src/App.jsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button"
+import React from 'react'
+import Sidebar from './components/ui/Sidebar'  // adjust the path according to your folder structure
 
-function App() {
+const App = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div>
+      <Sidebar />
     </div>
   )
 }
