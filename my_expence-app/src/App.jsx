8d56d690f0e6545e12fee3b668712b0a9f51a994
Fareedaasif083 <fareedaasif8130@gmail.com>
2025-12-13 09:56:09 +0500

    Add dashboard and expense tracker components

--- my_expence-app/src/App.jsx
+++ my_expence-app/src/App.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import { SidebarClose } from "lucide-react";
+// import Sidebar  from "./components/ui/Sidebar";
 
 function App() {
+   const [expenses, setExpenses] = useState([]);
+    const [filters, setFilters] = useState({ month: "all", category: "all" });
+
+    useEffect(() => {
+    const stored = localStorage.getItem("expenses");
+    if (stored) {
+      setExpenses(JSON.parse(stored));
+    }
+  }, []);
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
+      {/* <Sidebar/ expenses={expenses } filters={filters} onFilterChange={setFilters} > */}
       <main className="max-w-5xl mx-auto px-4 py-6">
-        
+        <Dashboard expenses={expenses} setExpenses={setExpenses} filters={filters}/>
       </main>
     </div>
   );
