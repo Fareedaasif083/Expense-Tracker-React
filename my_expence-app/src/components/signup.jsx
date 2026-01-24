9e66d1a9c2582ced9245f4354496f64a9be4856c
Fareedaasif083 <fareedaasif8130@gmail.com>
2026-01-24 12:39:13 +0500

    Save authentication work

---
+++ my_expence-app/src/components/signup.jsx
@@ -0,0 +1,60 @@
+
+
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { supabase } from "../config/supabaseClients";
+
+export default function Signup({ signupSuccess }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      signupSuccess(data.user);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex item-center justify-center">
+      <div className="w-full max-w-sm p-6 bg-gray-800  rounded-lg  shadow m-20  mb-30">
+        <h2 className="text-2xl font-semibold mt-5 text-center text-white">
+          Sign Up
+        </h2>
+        <form
+          onSubmit={handleSignup}
+          className="flex flex-col gap-3 mt-5 rounded-xl"
+        >
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className=" w-full mb-3.5 border border-white rounded text-white"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className=" w-full border border-white rounded mb-3.5 text-white"
+          />
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+          <Button className="bg-slate-900 text-gray-300 border rounded-4xl">
+            Sign Up
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
