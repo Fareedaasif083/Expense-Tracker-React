5ace881f9e678e15efc3e4dec91b1fa309de288a
Fareedaasif083 <fareedaasif8130@gmail.com>
2026-01-25 02:49:18 +0500

    Create Authentication.jsx

---
+++ my_expence-app/src/components/Authentication.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react" 
+import { Dialog, DialogTrigger, DialogContent, DialogTitle } from "@/components/ui/dialog" 
+import Login from "./Login" 
+import Signup from "./signup"
+
+export default function Authentication ({setUser}) {
+  const [showSignup,setShowSignup]=useState(false)
+
+  return(
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className="bg-slate-500 text-white rounded-4xl px-4 py-3">Signup/Login</button>
+      </DialogTrigger>
+      <DialogContent className="max-w-md bg-gray-800 text-white rounded-lg p-4">
+        <DialogTitle className="text-center text-2xl font-semibold mb-4">{showSignup ? "Sign Up":"Login"}</DialogTitle>
+        {showSignup ?( 
+         <>
+           <Signup signupSuccess= {setUser}/>
+           <p className="mt-2 text-sm text-center text-white">
+             Already have an account?{" "}
+             <button className="text-blue-500 underline" onClick={() => setShowSignup(false)}> Login </button>
+           </p>
+         </>
+      ):(
+      <>
+           <Login loginSuccess= {setUser}/>
+           <p className="mt-2 text-sm text-center text-white">
+             Don't have an account?{" "}
+             <button className="text-blue-500 underline" onClick={() => setShowSignup(true)}> Sign Up</button>
+           </p>
+         </>
+      )}
+      </DialogContent>
+    </Dialog>
+    );
+}
