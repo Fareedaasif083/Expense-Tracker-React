e98343180bb58c4895b21e25a5ef965ecaf9509c
Fareedaasif083 <fareedaasif8130@gmail.com>
2026-01-25 18:12:30 +0500

    Update Authentication.jsx

--- my_expence-app/src/components/Authentication.jsx
+++ my_expence-app/src/components/Authentication.jsx
@@ -19,15 +19,11 @@ export default function Authentication ({setUser}) {
       <DialogTrigger asChild>
         <button className="bg-slate-500 text-white rounded-4xl px-4 py-3">Signup/Login</button>
       </DialogTrigger>
-      <DialogContent className="max-w-md bg-gray-800 text-white rounded-lg p-4 flex flex-col items-center justify-center">
+      <DialogContent className="max-w-md bg-gray-800 text-white rounded-lg p-4">
         {showSignup ?( 
-         <>
            <Signup signupSuccess= {handleSuccessSignup} onFlip={() => setShowSignup(false)}/>
-         </>
       ):(
-      <>
            <Login loginSuccess= {handleSuccessLogin} onFlip={() => setShowSignup(true)}/>
-         </>
       )}
       </DialogContent>
     </Dialog>
