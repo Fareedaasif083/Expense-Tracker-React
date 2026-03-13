8edc8eb528e85eeb4b9d1b4e46442a509a0c8dde
Fareedaasif083 <fareedaasif8130@gmail.com>
2026-03-13 15:35:36 +0500

    Update chat.js

--- my_expence-app/api/chat.js
+++ my_expence-app/api/chat.js
@@ -68,6 +68,7 @@
 // });
 // }
 export default async function handler(req, res) {
+  console.log("Message received:", req.body.message); 
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
